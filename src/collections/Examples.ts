794f798bff45747a5bed180a2230a50eddae9f80
Thomas Smyth <thomas.smyth3141@gmail.com>
2022-09-01 09:18:46 +0100

    Add volume storage configuration

--- src/collections/Examples.ts
+++ src/collections/Examples.ts
@@ -3,6 +3,7 @@ import { CollectionConfig } from 'payload/types';
 // Example Collection - For reference only, this must be added to payload.config.ts to be used.
 const Examples: CollectionConfig = {
   slug: 'examples',
+  upload: { staticURL: '/uploads', staticDir: '/uploads' },
   admin: {
     useAsTitle: 'someField',
   },
